2f4853c3686aa44eb9de6d9465524e884733ed28
Bennie ferreira <bennieferreira.dev@gmail.com>
2024-04-18 00:29:23 -0300

    feature: adapter db for products finished

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -27,3 +27,45 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	}
 	return &product, nil
 }
+
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+	p.db.QueryRow("select if from products where id=?", product.GetId()).Scan(&rows)
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := p.update(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return product, nil
+}
+
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare(`insert into products(id, name, price, status) values(?, ?, ?, ?)`)
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmt.Exec(product.GetId(), product.GetName(), product.GetPrice(), product.GetStatus())
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec("update products set name = ?, price = ?, status = ? where id = ?",
+		product.GetName(), product.GetPrice(), product.GetStatus(), product.GetId())
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/codeedu/go-hexagonal/adapters/db"
+	"github.com/codeedu/go-hexagonal/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -50,4 +51,29 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "Product Test", product.GetName())
 	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
 }
